01e48cf45813fdef6f9ebe4235cdf89e14c51771
jhonathan vitor <jhonathanvitor@gmail.com>
2022-11-16 22:41:38 -0400

    Orders Modal

--- front-end/src/components/OrdersBoard/index.tsx
+++ front-end/src/components/OrdersBoard/index.tsx
@@ -1,29 +1,43 @@
+import { useState } from 'react';
+import { Order } from '../../types/Order';
+import { OrderModal } from '../OrderModal';
 import { Board, OrdersContainer } from './styles';
 
 interface OrderBoardProps {
   icon: string;
   title: string;
+  orders: Order[];
 }
 
-export function OrdersBoard({icon, title}: OrderBoardProps) {
+export function OrdersBoard({ icon, title, orders }: OrderBoardProps) {
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  function handleOpenModal() {
+    setIsModalVisible(true);
+  }
+
   return (
     <Board>
+
+      <OrderModal visible={isModalVisible}/>
+
       <header>
         <span>{icon}</span>
         <strong>{title}</strong>
         <samp>(1)</samp>
       </header>
 
-      <OrdersContainer>
-        <button type='button'>
-          <strong>Mesa 2</strong>
-          <span>2 itens</span>
-        </button>
-        <button type='button'>
-          <strong>Mesa 2</strong>
-          <span>2 itens</span>
-        </button>
-      </OrdersContainer>
+      {orders.length > 0 && (
+        <OrdersContainer>
+          {orders.map((order) => (
+            <button type='button' key={order._id} onClick={handleOpenModal}>
+              <strong>Mesa {order.table}</strong>
+              <span>{order.products.length} itens</span>
+            </button>
+          ))}
+        </OrdersContainer>
+      )}
     </Board>
   );
 }
